6b6b785ebbae188664b317a088e8aee2a4f9182f
Pedro Barrera <fiticostini@gmail.com>
2023-04-24 14:44:05 -0400

    Actualizando para pasar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 //create your first component
 
 const Home = () => {
-  const urlApi = "https://assets.breatheco.de/apis/fake/todos/";
+  const urlApi = "https://express-blog-xa7v.onrender.com/todo/users/aguilar";
 
   const [tareas, setTareas] = useState([]);
   const [tareaName, setTareaName] = useState("");
@@ -15,16 +15,31 @@ const Home = () => {
 
   const getTareas = async () => {
     try {
-      const response = await fetch(
-        "https://express-blog-xa7v.onrender.com/",
-        { headers:{"Content-Type": 'application/json'} }
+      const response = await fetch(urlApi,
+        { headers: { "Content-Type": 'application/json' } }
       );
       const data = await response.json();
+      setTareas(data);
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
+
+  //Actualizar tareas
+  const updateTareas = async (tareas) => {
+    try {
+      const response = await fetch(
+        urlApi,
+        { method: "PUT", headers: { "Content-Type": 'application/json' }, body: JSON.stringify(tareas) }
+      );
+      getTareas();
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getTareas();
   }, []);
@@ -33,25 +48,32 @@ const Home = () => {
     console.log(event.key);
     if (event.key === "Enter") {
       const newTarea = {
-        accion: tareaName,
+        label: tareaName,
+        done: true
       };
       const newTareas = [...tareas, newTarea];
-      setTareas(newTareas);
+      updateTareas(newTareas);
       setTareaName("");
     }
   };
   const handleAddClick = () => {
     const newTarea = {
-      accion: tareaName,
+      label: tareaName,
+      done: true
     };
     const newTareas = [...tareas, newTarea];
-    setTareas(newTareas);
+    updateTareas(newTareas);
     setTareaName("");
   };
 
-  const handleRemove = (id) => {
-    setTareas(tareas.filter((item, index) => index != id));
+  const handleRemove = (tareaindex) => {
+    if(tareas.length === 1) {
+      updateTareas([{label:"no-mostrar", done:true}])
+      return
+    }
+    updateTareas(tareas.filter((item, index) => index != tareaindex));
   };
+  console.log(tareas);
   return (
     <div className="d-flex justify-content-center">
       <div className="text-center w-50">
@@ -65,29 +87,35 @@ const Home = () => {
               onKeyDown={(event) => handleAddEnter(event)}
               value={tareaName}
             />
-            <button onClick={() => handleAddClick()}>Add me</button>
+            <button className="boton1" onClick={() => handleAddClick()}>Add me</button>
           </div>
           <div className="card-body">
-            {tareas.length === 0 && <p>there are no task</p>}
+            {tareas.length === 0 && <p>There are no task</p>}
             <ul className="list-group list-group-flush ">
               {tareas.map((tarea, index) => {
-                return (
-                  <li
-                    className="list-group-item d-flex justify-content-between"
-                    key={`${tarea.accion}-${index}`}
-                  >
-                    <p>{tarea.accion}</p>
-                    <button
-                      className="btn-close"
-                      aria-label="Close"
-                      onClick={() => handleRemove(index)}
-                    ></button>
-                  </li>
-                );
+                if(tarea.label !=="no-mostrar"){
+                  return (
+                    <li
+                      className="list-group-item d-flex justify-content-between"
+                      key={`${tarea.label}-${index}`}
+                    >
+                      <p>{tarea.label}</p>
+                      <button
+                        className="btn-close"
+                        aria-label="Close"
+                        onClick={() => handleRemove(index)}
+                      ></button>
+                    </li>
+                  );
+                }
+                
               })}
             </ul>
           </div>
-          <div className="card-footer">{tareas.length} item left</div>
+          <div className="card-footer d-flex justify-content-center">{tareas.length > 1 ? `${tareas.length - 1} item left`: "No Task"} </div>
+          <div className="d-flex justify-content-center">
+              <button className="boton1 col-2" onClick={() => updateTareas([{label:"no-mostrar", done:true}])}>All Delete</button>
+          </div>   
         </div>
       </div>
     </div>
